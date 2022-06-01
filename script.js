5540859fcaf4c0e46a0f1ca6e60f8cc35b606f34
AnBunchuk <av.bunchuk@gmail.com>
2022-06-01 22:35:36 +0300

    save3 добавил ф-ю удаления блока div все работает как надо

--- script.js
+++ script.js
@@ -18,20 +18,24 @@ class Plan {
 
     clickVid() {
         this.wrapper.addEventListener('click', (e) => {
-            console.dir(e)
+            // добавляем поле события нажатие на +
             if (e.target.className === 'btn icon-plus') {
                 this.creatDivTo()
             }
+            // помечаем событие выполненым нажатием на пустой круг
             if (e.target.className === 'btn' && this.divToValue) {
-                // console.log(this.divToValue)
                 this.creatDivDo(e)
-               let marker = this.divToList.querySelector('.to')
+                this.deleteDiv(e)
+                let marker = this.divToList.querySelector('.to')
+                //    если все события выполнены добавляем пустое поле событий в "Задачи"
                 if (!marker) {
                     this.creatDivTo()
                 }
             }
-
-
+            // удаляем элемент из поля "Выполненные задачи"
+            if (e.target.className === 'btn icon-down') {
+                this.deleteDiv(e);
+            }
         })
     }
 
@@ -39,8 +43,6 @@ class Plan {
     creatDivTo() {
         this.divElement = document.createElement('div')
         this.divElement.classList.add('to');
-        // console.log(divElement)
-        // console.log(this.divToList)
         this.divToList.append(this.divElement);
 
         let span1 = document.createElement('span');
@@ -62,7 +64,6 @@ class Plan {
     creatDivDo(e) {
         let divElement = document.createElement('div');
         divElement.classList.add('do');
-
         this.divDoList.append(divElement);
 
         let span1 = document.createElement('span');
@@ -81,17 +82,21 @@ class Plan {
         divElement.append(span2);
 
 
+        // получаем значение поля value из переносимого поля задачи
+        let divToValue = e.target.parentElement.children[1].value
+        input.setAttribute('value', divToValue)
+    }
 
-        let divToValu = e.target.parentElement.children[1].value
-        input.setAttribute('value', divToValu)
-
+    // удаляем элемент div
+    deleteDiv(e) {
         let elementDelete = e.target.parentElement
         elementDelete.remove()
     }
 
+
     init() {
         // console.dir( this.wrapper);
-
+        console.dir(localStorage)
         this.clickVid()
         this.inputVid()
         this.creatDivTo()
